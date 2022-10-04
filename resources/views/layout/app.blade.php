da2b042e24abd798b6590bb54a39934e77dd26fc
Abylay <abyllight@gmail.com>
2022-10-04 10:46:18 +0600

    add gtm to thankyou page

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -26,39 +26,25 @@
         })(window,document,'script','dataLayer','GTM-NC55Q45');</script>
     <!-- End Google Tag Manager -->
 
-    <!-- Meta Pixel Code -->
-    <script>
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '852043881844376');
-        fbq('track', 'PageView');
-    </script>
-    <noscript><img height="1" width="1" style="display:none"
-                   src="https://www.facebook.com/tr?id=852043881844376&ev=PageView&noscript=1"
-        /></noscript>
-    <!-- End Meta Pixel Code -->
-
     <meta name="mailru-domain" content="XLb4KEEntsFqbfyd" />
     @section('head')
     @show
 </head>
+
 <body class="font-body flex flex-col min-h-screen">
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NC55Q45"
-                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NC55Q45"
+                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     <section id="navbar">
         <navbar></navbar>
     </section>
+
     <main class="flex-grow">
         @yield('content')
     </main>
+
     <!-- Footer -->
     <footer id="footer" class="bg-gray-900 w-full">
         <div class="px-3 py-5 max-w-6xl mx-auto flex flex-col items-center justify-center">
